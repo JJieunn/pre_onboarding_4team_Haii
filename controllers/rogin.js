2383cc05107720d2c104182718cfde4eb5da095b
최정훈 <jeongssi94@gmail.com>
2022-10-09 18:25:29 +0900

    Refactor: 로그인 기능 수정 및 swaggerAPI추가

--- controllers/rogin.js
+++ controllers/rogin.js
@@ -3,7 +3,6 @@ const userService = require('../services/rogin');
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(email, password, 'sdasd');
     const { id, token } = await userService.login(email, password);
 
     return res.status(201).json({ id, token });
@@ -11,13 +10,5 @@ const login = async (req, res) => {
     return res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
-// const login = async (req, res) => {
-//   try {
-//     const token = await userService.login(req.body);
-//     res.status(201).json({ message: 'LOGIN SUCCESS', token });
-//   } catch (errors) {
-//     res.status(errors.statusCode || 500).json({ message: errors.message });
-//   }
-// };
 
 module.exports = { login };
