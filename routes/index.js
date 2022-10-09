05de20e691b7eadbf3a26027e62924ef3303d728
haeyeonleeee <haeyeon1796@gmail.com>
2022-10-09 14:27:23 +0900

    ADD swagger 추가

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { specs, swaggerUi } = require('../modules/swagger')
+const updatemgrRouter = require('./updatemgr')
 
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+router.use('/update', updatemgrRouter)
 
 module.exports = router;
 
