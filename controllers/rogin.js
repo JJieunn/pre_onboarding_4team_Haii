5ca3b22696fded296e6892c9fabe24c88e05ece0
최정훈 <jeongssi94@gmail.com>
2022-10-09 17:36:46 +0900

    fix:로그인 test

--- controllers/rogin.js
+++ controllers/rogin.js
@@ -1,23 +1,23 @@
 const userService = require('../services/rogin');
 
-// const loginController = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     console.log(email, password, 'sdasd');
-//     const { id, token } = await userService.login(email, password);
-
-//     return res.status(201).json({ id, token });
-//   } catch (error) {
-//     return res.status(error.statusCode || 500).json({ message: error.message });
-//   }
-// };
 const login = async (req, res) => {
   try {
-    const token = await userService.login(req.body);
-    res.status(201).json({ message: 'LOGIN SUCCESS', token });
-  } catch (errors) {
-    res.status(errors.statusCode || 500).json({ message: errors.message });
+    const { email, password } = req.body;
+    console.log(email, password, 'sdasd');
+    const { id, token } = await userService.login(email, password);
+
+    return res.status(201).json({ id, token });
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
+// const login = async (req, res) => {
+//   try {
+//     const token = await userService.login(req.body);
+//     res.status(201).json({ message: 'LOGIN SUCCESS', token });
+//   } catch (errors) {
+//     res.status(errors.statusCode || 500).json({ message: errors.message });
+//   }
+// };
 
 module.exports = { login };
